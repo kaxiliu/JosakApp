2ac6689bbbac4eb629d179d5a968e2f7895fccd2
Salet Diaz <saalet.diaaz@gmail.com>
2026-01-14 15:56:50 +0100

    Commit userDAO

---
+++ app/src/main/java/edu/josakapp/proyectoJosakapp/data/UserDAO.kt
@@ -0,0 +1,30 @@
+package edu.josakapp.proyectoJosakapp.data
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import edu.josakapp.proyectoJosakapp.data.model.Habito
+import edu.josakapp.proyectoJosakapp.data.model.User
+import edu.josakapp.proyectoJosakapp.data.model.UserWithHabito
+import edu.josakapp.proyectoJosakapp.data.model.UserWithPinguino
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDAO {
+
+    /**Para poder consultar los hábitos de un usuario en concreto*/
+    @Transaction
+    @Query("SELECT * FROM user ORDER BY nombre_usuario")
+    fun getUserWithHabito(): List<UserWithHabito>
+
+    @Query("SELECT * FROM habito")
+    fun getAllHabito(): Flow<List<UserWithHabito>>
+
+    @Query("SELECT * FROM user WHERE id_usuario = id_usuario")
+    suspend fun getUserById(id_usuario: Int): User?
+
+    @Query("SELECT * FROM habito WHERE id_habito = id_habito")
+    suspend fun getHabitoById(id_habito: Int): Habito?
+
+
+}
