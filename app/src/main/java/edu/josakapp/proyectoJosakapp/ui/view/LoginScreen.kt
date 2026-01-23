c9f7b506799d942f8ec2b840388669a7ef7833d0
salet diaz <saalet.diaaz@gmail.com>
2026-01-23 15:35:57 +0100

    Commit, borrado de imports innecesarios

--- app/src/main/java/edu/josakapp/proyectoJosakapp/ui/view/LoginScreen.kt
+++ app/src/main/java/edu/josakapp/proyectoJosakapp/ui/view/LoginScreen.kt
@@ -33,12 +33,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -46,7 +44,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import edu.josakapp.proyectoJosakapp.R
-import edu.josakapp.proyectoJosakapp.ui.theme.verdeNeon
 
 /**Pantalla encargada de el login del usuario*/
 @OptIn(ExperimentalGlideComposeApi::class)
