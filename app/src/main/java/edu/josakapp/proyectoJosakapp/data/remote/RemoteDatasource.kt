fd909fd8d0721ad35acc0aa740b9b83a4e8f20d3
Jorge <jorgebeneyto36@gmail.com>
2026-01-29 17:42:42 +0100

    creacion json

---
+++ app/src/main/java/edu/josakapp/proyectoJosakapp/data/remote/RemoteDatasource.kt
@@ -0,0 +1,11 @@
+package edu.josakapp.proyectoJosakapp.data.remote
+
+class RemoteDatasource (
+        private val api: RankingApi
+    ) {
+        suspend fun getRanking(): List<UserRanking> {
+            return api.getRanking()
+        }
+    }
+
+}
