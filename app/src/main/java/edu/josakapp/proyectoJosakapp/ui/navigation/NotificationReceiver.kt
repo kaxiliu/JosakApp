c70d2493449821cb7452df9c04d078854389d79c
Kaxi <liukaxi0413@gmail.com>
2026-02-06 11:46:17 +0100

    He arreglado el error de sincronización entre el widget y la app, y ahora se abre la app al hacer clic en el widget

--- app/src/main/java/edu/josakapp/proyectoJosakapp/ui/navigation/NotificationReceiver.kt
+++ app/src/main/java/edu/josakapp/proyectoJosakapp/ui/navigation/NotificationReceiver.kt
@@ -5,9 +5,7 @@ import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.widget.RemoteViews
 import android.widget.Toast
-import edu.josakapp.proyectoJosakapp.R
 import edu.josakapp.proyectoJosakapp.data.datasource.AppDatabase
 import edu.josakapp.proyectoJosakapp.data.model.HabitoRegistro
 import edu.josakapp.proyectoJosakapp.ui.components.HabitoWidget
@@ -15,34 +13,42 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class NotificationReceiver : BroadcastReceiver() {
+    // Se ejecuta cuando el usuario hace clic en el botón del Widget
     override fun onReceive(context: Context, intent: Intent) {
+        // Verifica si la acción es la correcta para actualizar el hábito
         if (intent.action == "ACTION_YES") {
+            // Obtiene el ID del hábito
             val habitoId = intent.getIntExtra("HABITO_ID", 1)
+            if (habitoId == -1) return
 
+            // Inicia una corrutina para realizar operaciones de base de datos en segundo plano
             GlobalScope.launch {
                 val db = AppDatabase.getInstance(context)
                 val currentHabito = db.habitosDAO().getHabitoById(habitoId)
+                val hoy = java.time.LocalDate.now() // Calcula el tiempo de hoy
+                    .atStartOfDay(java.time.ZoneId.systemDefault())
+                    .toInstant()
+                    .toEpochMilli()
+
 
                 if (currentHabito != null) {
-                    val nuevoEstado = !currentHabito.estado
+                    val nuevoEstado = !currentHabito.estado // Cambia el estado
                     db.habitosDAO().updateEstado(habitoId, nuevoEstado)
-
+                    // Si ahora está completado, inserta un registro; si no, lo elimina
                     if (nuevoEstado) {
-                        db.habitosDAO().insertRegistro(HabitoRegistro(habitoId, System.currentTimeMillis()))
+                        db.habitosDAO().insertRegistro(HabitoRegistro(habitoId, hoy))
+                    }else {
+                        db.habitosDAO().deleteRegistro(habitoId, hoy)
                     }
 
+                    // Actualización inmediata del Widget
                     val appWidgetManager = AppWidgetManager.getInstance(context)
                     val componentName = ComponentName(context, HabitoWidget::class.java)
-                    val views = RemoteViews(context.packageName, R.layout.widget_layout)
-
-                    if (nuevoEstado) {
-                        views.setTextViewText(R.id.widget_habito_nombre, " ${currentHabito.nombre}")
-                        views.setTextViewText(R.id.btn_widget_cumplir, "Deshacer")
-                    } else {
-                        views.setTextViewText(R.id.widget_habito_nombre, currentHabito.nombre)
-                        views.setTextViewText(R.id.btn_widget_cumplir, "Cumplir")
+                    val appWidgetIds = appWidgetManager.getAppWidgetIds(componentName)
+                    // Llama  updateWidget para refrescar la interfaz visual
+                    for (id in appWidgetIds) {
+                        HabitoWidget.updateWidget(context, appWidgetManager, id)
                     }
-                    appWidgetManager.updateAppWidget(componentName, views)
                 }
             }
             Toast.makeText(context, "Estado actualizado", Toast.LENGTH_SHORT).show()
