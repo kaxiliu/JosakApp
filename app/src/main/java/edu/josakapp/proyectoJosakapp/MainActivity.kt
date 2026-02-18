7c2ee125c50c6a72e056b5fcd68a774ecbf0d45d
salet diaz <saalet.diaaz@gmail.com>
2026-02-18 21:41:28 +0100

    quite un import

--- app/src/main/java/edu/josakapp/proyectoJosakapp/MainActivity.kt
+++ app/src/main/java/edu/josakapp/proyectoJosakapp/MainActivity.kt
@@ -6,14 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
